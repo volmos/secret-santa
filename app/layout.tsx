40173b5bfd0ed4eb3136dca2e0ed86ccc33eca18
victor <v.olmos@weguest.com>
2024-12-15 16:11:06 +0100

    Style changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,18 +26,20 @@ export default function RootLayout({
     children: ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="es">
         <body
             className={`${monserrat.variable} ${yeseva.variable}  font-[family-name:var(--font-monserrat)] antialiased text-primary`}
         >
-        <div className="max-w-2xl mx-auto h-screen flex flex-col">
-            <div className="w-full aspect-video bg-[url('/socks.svg')] bg-no-repeat bg-cover"></div>
+        <div
+            className="max-w-3xl mx-auto min-h-full md:min-h-[calc(100vh-1rem)] bg-[url('/background.svg')] bg-contain flex flex-col md:border-8 border-white md:rounded-3xl shadow-2xl md:my-2 bg-background overflow-hidden">
+            <header className="w-full bg-[url('/socks.svg')] bg-no-repeat bg-contain bg-top">
+                <H1 className="text-center mt-[46%] mb-4">Amigo invisible</H1>
+            </header>
             <div className="px-6 flex-grow flex flex-col">
-                <H1 className="text-center mb-4">Amigo invisible</H1>
                 {children}
             </div>
-            <footer className="text-center p-8 text-xs">
-                Víctor Olmos &copy; {new Date().getFullYear()}
+            <footer className="w-full bg-[url('/sled.svg')] bg-no-repeat bg-cover bg-top mt-8">
+                <div className="text-center uppercase mt-[78%]">Víctor Olmos &copy; {new Date().getFullYear()}</div>
             </footer>
         </div>
         </body>
